52363d9de5d78fd0fb85e73d6b7525e5798dab0b
Mano <Koutouzosm@gmail.com>
2019-04-24 08:55:58 -0400

    added more to readMe

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # liri-node-app
+
+##Built With
+-node.js
+-javascript
+
+##API's Used
+-Spotify API
+-Bands in Town A:I
+-omDB API
+
+###What is it?
+A node application used to find information on music, movies, and can also find venues in an area near you by simply entering your favorite music, movie, or bands!!
+
+###How does it work?
+This program will run in your command line only. Using the command line find where you saved the program.  When in that program you have four different commands you can use to enter your search.
+    -Spotify-this-song "Song Goes Here"
+    -movie-this "Movie Goes Here"
+    -concert-this "Band/Artist Goes Here"
+    -do-what-it-says 'takes information out of .txt file and returns info'
--- liri.js
+++ liri.js
@@ -7,124 +7,134 @@ var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 
 
+
 var request = process.argv[2]
 var userInput = process.argv.slice(3).join(" ")
 
 // use switch statement to figureout what function to run based on the value of "request"
 switch (request) {
-    case "spotify-this-song":
-      spotifyThis();
-      break;
-    case "movie-this":
-      omdbThis();
-      break;
-    case "concert-this":
-      bitThis();
-      break;
-    case "do-what-it-says":
-      doIt();
-      break;
+  case "spotify-this-song":
+    spotifyThis();
+    break;
+  case "movie-this":
+    omdbThis();
+    break;
+  case "concert-this":
+    bitThis();
+    break;
+  case "do-what-it-says":
+    doIt();
+    break;
+}
+
+// "spotify-this-song" => use node-spotify-api to search for tracks in userInput
+function spotifyThis() {
+  if (userInput === ""){
+    userInput = "The Sign"
+  }
+  spotify.search({ type: `track`, query: `${userInput}` }, function (error, data) {
+    if (error) {
+      console.log(error)
+    } 
+    else{
+      console.log("========================================")
+      console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+      console.log(`Title: ${data.tracks.items[0].name}`);
+      console.log(`Preview: ${data.tracks.items[0].preview_url}`);
+      console.log(`Album: ${data.tracks.items[0].album.name}`);
+      console.log("========================================")
     }
-   
-     // "spotify-this-song" => use node-spotify-api to search for tracks in userInput
-    function spotifyThis() {
-      spotify.search({type: "track", query: `${userInput}`}, function(error, data){
-        if (error){
-          console.log(error)
-        } else {
-          console.log("========================================")
-          console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
-          console.log(`Title: ${data.tracks.items[0].name}`);
-          console.log(`Preview: ${data.tracks.items[0].preview_url}`);
-          console.log(`Album: ${data.tracks.items[0].album.name}`);
-          console.log("========================================")
-        }
+  })
+};
+
+
+// "movie-this" => use axios.get() to hit the omdb api to search for whatever movie is listed in userInput
+function omdbThis() {
+  if (userInput === "") {
+    axios.get(`http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy`).then(
+      function (response) {
+        console.log("========================================")
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.Ratings[0]);
+        console.log(response.data.Ratings[1]);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+        console.log("========================================")
       }
-      )};
-    
-    
-      // "movie-this" => use axios.get() to hit the omdb api to search for whatever movie is listed in userInput
-    function omdbThis() {
-      if(userInput===""){
-        axios.get(`http://www.omdbapi.com/?t=mr+nobody&apikey=trilogy`).then(
-          function(response){
-            console.log("========================================")
-            console.log(response.data.Title);
-            console.log(response.data.Year);
-            console.log(response.data.Ratings[0]);
-            console.log(response.data.Ratings[1]);
-            console.log(response.data.Country);
-            console.log(response.data.Language);
-            console.log(response.data.Plot);
-            console.log(response.data.Actors);
-            console.log("========================================")
-          }
-        )
-      } else {
-      axios.get(`http://www.omdbapi.com/?t=${userInput}&apikey=trilogy`).then(
-        function (response) {
-          console.log("========================================")
-          console.log(response.data.Title);
-          console.log(response.data.Year);
-          console.log(response.data.Ratings[0]);
-          console.log(response.data.Ratings[1]);
-          console.log(response.data.Country);
-          console.log(response.data.Language);
-          console.log(response.data.Plot);
-          console.log(response.data.Actors);
-          console.log("========================================")
-        }
-      )
+    )
+  } else {
+    axios.get(`http://www.omdbapi.com/?t=${userInput}&apikey=trilogy`).then(
+      function (response) {
+
+        console.log("========================================")
+        console.log(response.data.Title);
+        console.log(response.data.Year);
+        console.log(response.data.Ratings[0]);
+        console.log(response.data.Ratings[1]);
+        console.log(response.data.Country);
+        console.log(response.data.Language);
+        console.log(response.data.Plot);
+        console.log(response.data.Actors);
+        console.log("========================================")
+
       }
-    }
-    
-    // "concert-this" => use axios.get() to hit the bandsintown api to search for concerts in userInput
-    function bitThis() {
-      axios.get(`https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`).then(
-        function(response){
-        for (var i = 0; i < response.data.length; i ++){
-          console.log("========================================")
+    )
+  }
+}
+
+// "concert-this" => use axios.get() to hit the bandsintown api to search for concerts in userInput
+function bitThis() {
+  console.log(userInput);
+  axios.get(`https://rest.bandsintown.com/artists/${userInput.trim()}/events?app_id=codingbootcamp`).then(
+    function (response) {
+      for (var i = 0; i < response.data.length; i++) {
+
+        console.log("========================================")
         console.log(response.data[i].venue.name);
         console.log(response.data[i].venue.city);
-        console.log(response.data[i].datetime);
+        console.log(moment(response.data[i]).format("MM-DD-YYYY"));
         console.log("========================================")
-        }
-        }
-      )
+      }
     }
-    
+  )
+}
 
-    // "do-what-it-says" => use 'fs' package to read random.txt, split the data in there into an Array, then feed that array[0] into the function
-    function doIt() {
-      fs.readFile("./random.txt", "utf8", function (error, data) {
 
-      
-        dataArr = data.split(",");
-        
-        result = dataArr[0];
-        
-        userInput = dataArr[1];
+// "do-what-it-says" => use 'fs' package to read random.txt, split the data in there into an Array, then feed that array[0] into the function
+function doIt() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
 
-        switch (dataArr[0]) {
-          case "concert-this":
-              bitThis(userInput);
-              break;
-          case "spotify-this-song":
-              spotifyThis(userInput);
-              break;
-          case "movie-this":
-              ombdThis(userInput);
-              break;
-        }
-        // checking for err
-        if (error) {
-          return console.log(err);
-        }
-      })
+    // checking for err
+    if (error) {
+      return console.log(err);
     }
 
+    dataArr = data.split(",");
 
-  
+    result = dataArr[0];
+
+    userInput = dataArr[1];
+
+    switch (dataArr[0]) {
+      case "concert-this":
+        bitThis(userInput);
+        break;
+      case "spotify-this-song":
+        spotifyThis(userInput);
+        break;
+      case "movie-this":
+        ombdThis(userInput);
+        break;
+    }
+
+  })
+}
+
+
+
 
 
 
